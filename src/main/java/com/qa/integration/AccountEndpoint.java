5a30ea747211afac32ea62c14099ff4ba7d40a53
DeclandHorlick <deco-9-deco@live.ie>
2018-02-26 15:04:39 +0000

    Project Build

---
+++ src/main/java/com/qa/integration/AccountEndpoint.java
@@ -0,0 +1,5 @@
+package com.qa.integration;
+
+public class AccountEndpoint {
+
+}
--- src/main/java/com/qa/integration/JAXActivator.java
+++ src/main/java/com/qa/integration/JAXActivator.java
@@ -1,5 +1,8 @@
 package com.qa.integration;
 
-public class JAXActivator {
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
+@ApplicationPath("rest")
+public class JAXActivator extends Application {
 }
